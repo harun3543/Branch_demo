993bf143081d7f7c450baea30843443cb12ac179
harun3543 <harun.3543@gmail.com>
2020-09-03 17:07:01 +0300

    Carpma ozeeligi eklendi.

--- branch.php
+++ branch.php
@@ -9,5 +9,14 @@ function topla() {
   return $toplam;
 }
 
+function carpma() {
+  $carpim = 1;
+  $argumanSayisi = func_num_args();
+  for ($i = 0; $i < $argumanSayisi; $i ++) {
+    $carpim *= func_get_arg($i);
+  }
+  return $carpim;
+}
+
 // Ornek Kullanim
-// echo topla(5, 10, 15); // 30 dondurur
+// echo carpma(2, 3, 4); // 24 dondurur
